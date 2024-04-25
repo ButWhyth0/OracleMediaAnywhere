913a54be07976584b8637525db33308abf421802
Furqan Rizwan <furqanrizwan99@gmail.com>
2024-04-25 23:17:56 +0100

    A working version of the CasparCG extension that now takes in the CG number, and the desired layer

--- Oracle_Chrome_Extension/background.js
+++ Oracle_Chrome_Extension/background.js
@@ -1,125 +1,48 @@
-
 chrome.runtime.onInstalled.addListener( () => {
     // Defining the item in the right-click menu
     chrome.contextMenus.create({
         "id": "sendToCaspar",
         "title": "Send To Caspar",
-        "contexts": ["image", "selection"] // Context Menu item will appear when right-clicking images and highlighted text
+        "contexts": ["image"] // Context Menu item will appear when right-clicking images and highlighted text
     });
 })
+
+
 
 function escapeHtml(unsafe) {
     return unsafe.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g,
         "&#039;");
 }
 
+
+
+
 // A listener for when 'send to caspar' is clicked
 chrome.contextMenus.onClicked.addListener( (info, tab) => {
-    // console.log(info)
+    // If the clicked dropdown item isn't the extension just exit
     if(info.menuItemId !== "sendToCaspar") return
 
-    if(info.srcUrl) {
-        handleData('image', info.srcUrl) // Data is sent to another function for processing/ handling
-    }
-
-    if(info.selectionText) {
-        chrome.scripting.executeScript({
-            target: {tabId: tab.id},
-            function: (selectedText) => {
-                // Gets the selected HTML
-                const range = document.getSelection().getRangeAt(0);
-                const div = document.createElement('div');
-                div.appendChild(range.cloneContents())
-
-                // Removes all the unnecessary HTML that isn't need for the definition
-                div.querySelector('sup') ? div.querySelector('sup').remove() : div ;
-                div.querySelectorAll('[role="list"]').forEach(junkElement => junkElement.parentElement.parentElement.remove())
-                div.querySelectorAll('a[tabIndex="-1"]').forEach(junkElement => junkElement.parentElement.parentElement.remove())
-                div.querySelectorAll('span.kqEaA').forEach(junkElement => junkElement.parentElement.parentElement.remove())
-                div.querySelectorAll('a').forEach(link => {link.outerHTML = link.innerText})
-
-                return div.innerHTML;
-            },
-            args: [info.selectionText],
-        }, (result) => {
-            console.log(result[0].result)
-            handleData('text', escapeHtml(result[0].result)) // Data is sent to another function for processing/ handling
-        })
-    }
-})
-
-// // Default values for ip, port, channel, and layer
-// var cg_ip = '192.168.70.9';
-// var cg_port = 8080;
-// var cg_channel = 2;
-// var cg_layer = 60;
-
-// // Restores previously saved options
-// const restoreOptions = () => {
-//     chrome.storage.sync.get(
-//         // default values, needed otherwise these variables will be undefined
-//         {cgIP: "192.168.70.9", cgPort: 8080, cgChannel: 2, cgLayer: 60},
-//         (items) => {
-//             console.log('OPTIONS RESTORED')
-//             cg_ip = items.cgIP;
-//             cg_port = items.cgPort;
-//             cg_channel = items.cgChannel;
-//             cg_layer = items.cgLayer;
-//             console.log('cgIP: ',cg_ip)
-//             console.log('cgPORT: ',cg_port)
-//             console.log('cgCHANNEL: ',cg_channel)
-//             console.log('cgLAYER: ',cg_layer)
-//         }
-//     )
-// }
-
-
-// // Handles the data that is being sent
-// async function handleData(type, data) {
-//     try {
-//         await restoreOptions()
-//         const response = await fetch(`http://192.168.1.58:8080/casparcg2/api/showSelection?type=${type}`,{
-//             method: 'POST',
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({"data": data,"channel": 2, "layer": 60})
-//         })
-//     } catch (error) {
-//         console.error('THERE WAS AN ERROR:\n',error)
-//     }
-// }
-
-
-// Handles the data that is being sent
-const handleData = async(type,data) => {
+    // Retrieves saved extension settings to process the data
     chrome.storage.sync.get(
-        {cgIP: "", cgPort: 0, cgChannel: 0, cgLayer: 0},
+        // Dummy data values, just need the names to retrieve their data from storage
+        {cgLayer: 60, cgSelection: 6},
         async (items) => {
-            var cg_ip= items.cgIP;
-            var cg_port = items.cgPort;
-            var cg_channel = items.cgChannel;
-            var cg_layer = items.cgLayer;
+            console.log('cgLayer: ',items.cgLayer,'\ncgSelection: ',items.cgSelection)
 
-            // Try-catch block is inside the storage-get block so that the updated information is sent
+            // Calls the casparcg nodejs middleware
             try {
-                console.log('cgIP: ',cg_ip)
-                console.log('cgPORT: ',cg_port)
-                console.log('cgCHANNEL: ',cg_channel)
-                console.log('cgLAYER: ',cg_layer)
-                const response = await fetch(`https://${cg_ip}:${cg_port}/casparcg2/api/showSelection?type=${type}`,
+                const response = await fetch(`http://192.168.70.9:8080/casparcg/api/showImage?CG=${items.cgSelection? items.cgSelection : 6}`, // If there is no CG[Number], it will default to CG6
                 {
                     method: 'POST',
                     headers: {
                         "Content-Type": "application/json"
                     },
-                    body: JSON.stringify({"data": data, "channel": cg_channel, "layer": cg_layer})
-                }
-            )
+                    body: JSON.stringify({"url": info.srcUrl, layer: items.cgLayer})
+                })
             } catch (error) {
-                console.error('THERE WAS AN ERROR CALLING THE API:\n',error)
-                
+                console.error('THERE WAS AN ERROR FETCHING API\n')
+                console.error(error)
             }
         }
     )
-}
+})
--- Oracle_Chrome_Extension/options/options.js
+++ Oracle_Chrome_Extension/options/options.js
@@ -1,18 +1,11 @@
-// default values
-const ip_default = "192.168.70.9";
-const port_default = 5250;
-const channel_default = 2;
-const layer_default = 60;
-
 // Saves options to chrome.storage
 const saveOptions = () => {
-    const ip = document.getElementById("cg_ip").value
-    const port = document.getElementById("cg_port").value
-    const channel = document.getElementById("cg_channel").value
-    const layer = document.getElementById("cg_layer").value
+    const channel = document.getElementById("cg_channel").value;
+    const layer = document.getElementById("cg_layer").value;
+    const selection = document.getElementById("cg_selection").value;
 
     chrome.storage.sync.set(
-        {cgIP: ip, cgPort: port, cgChannel: channel, cgLayer: layer},
+        {cgLayer: layer, cgSelection: selection},
         () => {
             console.log('OPTIONS SAVED')
             // Updates status to let user know that the changes have been saved
@@ -28,13 +21,11 @@ const saveOptions = () => {
 // Restores previously saved options
 const restoreOptions = () => {
     chrome.storage.sync.get(
-        {cgIP: "192.168.70.9", cgPort: 5250, cgChannel: 2, cgLayer: 60},
+        {cgLayer: 60, cgSelection: 1},
         (items) => {
             console.log('OPTIONS RESTORED')
-            document.getElementById("cg_ip").value = items.cgIP;
-            document.getElementById("cg_port").value = items.cgPort;
-            document.getElementById("cg_channel").value = items.cgChannel;
             document.getElementById("cg_layer").value = items.cgLayer;
+            document.getElementById("cg_selection").value = items.cgSelection;
         }
     )
 }
